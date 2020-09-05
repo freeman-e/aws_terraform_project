95dbb2938cb77a9009b57eb8c4b8e26af7138db4
freeman-e <70806845+freeman-e@users.noreply.github.com>
2020-09-05 16:41:28 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,9 @@
- resource "aws_vpc" "vpc" {
+ provider "aws" {
+      region     = var.region
+      access_key = var.access_key
+      secret_key = var.secret_key
+ } 
+resource "aws_vpc" "vpc" {
      cidr_block = "10.0.1.0/24"
 
      tags = {
